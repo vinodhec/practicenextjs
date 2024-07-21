df4655579acf06b3551d437bf4554e02601340ae
Vinodh Thangavel <vinodhec@gmail.com>
2024-07-21 18:39:13 +0530

    Refactor about page component in src/app/about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import Head from "next/head";
+
 export default function () {
-  return <p>My name is Vinodh, I love my son Pranav very much</p>;
+  return (
+    <div>
+      <Head>
+        <title>This is about page</title>
+        <meta name="description" content="This is about page"></meta>
+      </Head>
+      <h1>About Vinodh Thangavel</h1>
+      <p>My name is Vinodh, I love my son Pranav very much</p>
+
+      <style jsx>{`
+        div {
+          text-align: center;
+          margin-top: 50px;
+        }
+        h1 {
+          color: blue;
+        }
+      `}</style>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello, Next.js!</p>;
+  return (
+    <div>
+      <Head>
+        <title>Home page</title>
+        <meta
+          name="description"
+          content="Welcome to Next.js practice home page"
+        ></meta>
+      </Head>
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to the practice home page</p>
+
+      <style jsx>{`
+        div {
+          text-align: center;
+          margin-top: 50px;
+        }
+        h1 {
+          color: grey;
+        }
+      `}</style>
+    </div>
+  );
 }
